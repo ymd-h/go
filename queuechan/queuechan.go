198fdc93df03dbfe9265192e8084fdfc7e0d31ee
Yamada <ymd_h@outlook.jp>
2022-12-13 20:04:06 +0900

    Add queuechan module for Queue Channel

---
+++ queuechan/queuechan.go
@@ -0,0 +1,71 @@
+// queuechan package
+//
+// Queue based infinite length channel
+package queuechan
+
+import (
+	"context"
+)
+
+// Create new channel set
+//
+// # Arguments
+// * `ctx`: `context.Context` - Context to stop Queue Channnel
+//
+// # Returns
+// * `in`: `chan <- T` - Input channel to Queue
+// * `out`: `<- chan T` - Output channel from Queue
+//
+// # Context
+// * `Done()` -> Don't take out from `in` any more, but do put to `in` for cleanup.
+func New[T any](ctx context.Context) (chan <- T, <- chan T) {
+	in := make(chan T, 0)
+	out := make(chan T, 0)
+
+	go func(in <- chan T, out chan <- T){
+		defer close(out)
+		queue := make([]T, 0)
+
+		LOOP:
+		for {
+			var v T
+			var ok bool
+			if len(queue) == 0 {
+				switch {
+				case <- ctx.Done():
+					return
+				case v, ok = <- in:
+					if !ok {
+						// `in` is closed
+						// No cleanup is needed
+						return
+					}
+					queue = append(queue, v)
+				}
+			}
+
+			switch {
+			case <- ctx.Done():
+				break LOOP
+			case v, ok = <- in:
+				if ok {
+					queue = append(queue, v)
+				} else {
+					// `in` is closed
+					// Go to cleanup
+					break LOOP
+				}
+			case out <- queue[0]:
+				queue = queue[1:]
+			}
+		}
+
+		// Clean up
+		for len(queue) > 0 {
+			out <- queue[0]
+			queue = queue[1:]
+		}
+	}(in, out)
+
+	return in, out
+}
