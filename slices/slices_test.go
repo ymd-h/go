a815eb28c98d8f5b8637fcdc1e7ec45ebecd3fc6
Yamada <ymd_h@outlook.jp>
2022-12-17 15:48:14 +0900

    Add test case for NewSlice

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -17,6 +17,21 @@ func TestNewSlice(t *testing.T) {
 			arg: 1,
 			want: NewComparableSlice[int](),
 		}).
+		Add("string", test{
+			arg: "a",
+			want: NewComparableSlice[string](),
+		}).
+		Add("uint", test{
+			arg: uint(1),
+			want: NewComparableSlice[uint](),
+		}).
+		Add("*int (unsupported)", test{
+			arg: func() *int {
+				i := 1
+				return &i
+			}(),
+			want: &Slice[*int]{ item: []*int{} },
+		}).
 		Run(func (_ *testing.T, data test) {
 			wantName := reflect.TypeOf(data.want).Name()
 			var got string
