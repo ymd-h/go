7849f6e3dc93302488ad1c9326ab51319b640a51
Yamada <ymd_h@outlook.jp>
2023-10-15 18:05:10 +0900

    Add resolve subpackage as dependency resolver

---
+++ worktidy/resolve/resolve.go
@@ -0,0 +1,40 @@
+package resolve
+
+import (
+	"fmt"
+
+	"github.com/ymd-h/go/sets"
+)
+
+type (
+	DepsSets = map[string]sets.Set[string]
+)
+
+
+func Resolve(unresolved DepSets) (DepSets, error) {
+	resolved := DepSets{}
+
+	for mod, deps := range unresolved {
+		stack := deps.ToSlice()
+
+		for len(stack) > 0 {
+			d = stack[0]
+			stack = stack[1:]
+
+			if u, ok := unresolved[d]; ok {
+				for _, ud := range u.ToSlice() {
+					if !deps.Has(ud) {
+						deps.Add(ud)
+						stack = append(stack, ud)
+					}
+				}
+			} else {
+				return nil, fmt.Errorf("Unknown Dependency: %s", d)
+			}
+		}
+
+		resolved[mod] = deps
+	}
+
+	return resolved, nil
+}
