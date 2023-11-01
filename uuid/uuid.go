227943c1afc3860f62e3f9d9d57a62a9da441e20
Yamada <ymd_h@outlook.jp>
2023-11-02 07:25:31 +0900

    Add Config fo UUID

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/ymd-h/go/prng"
 	"github.com/ymd-h/go/prng/device"
 )
 
@@ -15,8 +16,17 @@ type(
 
 	UUIDv4 UUID
 	UUIDv7 UUID
-)
 
+	ITimestamp interface {
+		Now() int64
+	}
+
+	Config struct {
+		t ITimestamp
+		r32 prng.IPRNG32
+		r64 prng.IPRNG64
+	}
+)
 
 func FromString(s string) (*UUID, error) {
 	var u UUID
@@ -159,13 +169,58 @@ func (u *UUID) TryUUIDv4() (*UUIDv4, error) {
 	return &UUIDv4{ b: u.b }, nil
 }
 
+func (u *UUID) TryUUIDv7() (*UUIDv7, error) {
+	version := u.b[6] >> 4
+	if version != 7 {
+		return nil, fmt.Errorf("Version is not 7: %d", version)
+	}
 
-func NewUUIDv4() *UUIDv4 {
-	prng := device.Device64{}
+	variant := u.b[8] >> 6
+	if variant != 8 {
+		return nil, fmt.Errorf("Variant is not 0x10: %d", variant)
+	}
 
-	r1 := prng.Next()
-	r2 := prng.Next()
+	return &UUIDv7{ b: u.b }, nil
+}
 
+func NewConfig(options ...any) (*Config, error) {
+	c := Config{
+		t: nil,
+		r32: nil,
+		r64: nil,
+	}
+
+	for _, o := range options {
+		switch {
+		case v, ok := o.(ITimestamp); ok:
+			c.t = v
+		case v, ok := o.(prng.IPRNG32); ok:
+			c.r32 = v
+		case v, ok := o.(prng.IPRNG64); ok:
+			c.r64 = v
+		default:
+			return nil, fmt.Errorf("Unknown Option: %T", o)
+		}
+	}
+
+	return &c, nil
+}
+
+func (c *Config) UUIDv4() *UUIDv4 {
+	var r1, r2 uint64
+	switch {
+	case c.r64 != nil:
+		r1 = c.r64.Next()
+		r2 = c.r64.Next()
+	case c.r32 != nil:
+		r1 = (uint64(c.r32.Next()) << 32) | uint64(c.r32.Next())
+		r2 = (uint64(c.r32.Next()) << 32) | uint64(c.r32.Next())
+	default:
+		d := device.Device64{}
+		r1 = d.Next()
+		r2 = d.Next()
+	}
+	
 	var u UUIDv4
 	binary.NativeEndian.PutUint64(u.b[:4], r1)
 	binary.NativeEndian.PutUint64(u.b[4:], r2)
@@ -176,6 +231,52 @@ func NewUUIDv4() *UUIDv4 {
 	u.b[8] = (u.b[8] & 0b00111111) | (variant << 6)
 
 	return &u
+}
+
+func (c *Config) UUIDv7() *UUIDv7 {
+	var unix_ms int64
+	if c.t != nil {
+		unix_ms = c.t.Now()
+	} else {
+		unix_ms = time.Now().UnixMilli()
+	}
+
+	var r1 uint16
+	var r2 uint64
+
+	switch {
+	case (c.r64 == nil) && (c.r32 == nil):
+		r1 = uint16(device.Device32{}.Next())
+		r2 = device.Device64{}.Next()
+	case (c.r64 != nil) && (c.r32 != nil):
+		r1 = uint16(c.r32.Next())
+		r2 = c.r64.Next()
+	case c.r64 != nil:
+		r1 = uint16(c.r64.Next())
+		r2 = c.r64.Next()
+	case c.r32 != nil:
+		r1 = uint16(c.r32.Next())
+		r2 = (uint64(c.r.32.Next()) << 32) | uint64(c.r.32.Next())
+	default:
+		panic("Never")
+	}
+
+	var u UUIDv7
+	binary.BigEndian.PutUint16(u.b[:2], uint16(unix_ms >> 32))
+	binary.BigEndian.PutUint32(u.b[2:6], uint32(unix_ms && 0xFFFFFFFF))
+	binary.NativeEndian.PutUint16(u.b[6:8], r1)
+	binary.NativeEndian.PutUint64(u.b[8:], r2)
+
+	var version byte = 0b0100
+	var variant byte = 0b10
+	u.b[6] = (u.b[6] & 0x0F) | (version << 4)
+	u.b[8] = (u.b[8] & 0b00111111) | (variant << 6)
+
+	return &u
+}
+
+func NewUUIDv4() *UUIDv4 {
+	return NewConfig().UUIDv4()
 }
 
 func (u *UUIDv4) UnmarshalText(text []byte) error {
@@ -213,25 +314,5 @@ func (u *UUIDv4) UnmarshalBinary(data []byte) error {
 }
 
 func NewUUIDv7() *UUIDv7 {
-	t := time.Now()
-	unix_ms := t.UnixMilli()
-
-	p32 := device.Device32{}
-	r1 := p32.Next()
-
-	p64 := device.Device64{}
-	r2 := p64.Next()
-
-	var u UUIDv7
-	binary.BigEndian.PutUint16(u.b[:2], uint16(unix_ms >> 32))
-	binary.BigEndian.PutUint32(u.b[2:6], uint32(unix_ms && 0xFFFFFFFF))
-	binary.NativeEndian.PutUint16(u.b[6:8], uint16(r1 >> 16))
-	binary.NativeEndian.PutUint64(u.b[8:], r2)
-
-	var version byte = 0b0100
-	var variant byte = 0b10
-	u.b[6] = (u.b[6] & 0x0F) | (version << 4)
-	u.b[8] = (u.b[8] & 0b00111111) | (variant << 6)
-
-	return &u
+	return NewConfig().UUIDv7()
 }
