31b183cb4ca5ba15914f42175010fe03d8142976
Yamada <ymd_h@outlook.jp>
2023-11-12 21:59:46 +0900

    Test for shutdown

--- async/worker_test.go
+++ async/worker_test.go
@@ -75,3 +75,37 @@ func TestWorker(t *testing.T){
 		})
 	}
 }
+
+
+func TestShutdown(t *testing.T){
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	tests := []struct{
+		name string
+		w IWorker
+	}{
+		{
+			name: "Worker",
+			w: NewWorker(ctx, 2),
+		},
+		{
+			name: "LazyWorker",
+			w: NewLazyWorker(ctx, 2),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(*testing.T){
+			_, err := RunAtWorker(
+				context.Background(),
+				test.w,
+				func() struct {} { return struct{}{} },
+			)
+			if err == nil {
+				t.Errorf("Must Fail")
+				return
+			}
+		})
+	}
+}
