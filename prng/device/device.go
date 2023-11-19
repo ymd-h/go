e5052805238927822eab107617d08585dac77971
Yamada <ymd_h@outlook.jp>
2023-11-19 22:07:12 +0900

    Improve device random

--- prng/device/device.go
+++ prng/device/device.go
@@ -12,19 +12,20 @@ type (
 
 
 func (_ Device32) Next() uint32 {
-	m := big.NewInt(0x01_00_00_00_00)
-	s, _ := rand.Int(rand.Reader, m)
-	return uint32(s.Uint64())
+	b := make([]byte, 4)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return binary.NativeEndian.Uint32(b)
 }
 
 
 func (_ Device64) Next() uint64 {
-	m := big.NewInt(0)
-	m.SetBytes([]byte{
-		1,
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-	})
-	s, _ := rand.Int(rand.Reader, m)
-	return s.Uint64()
+	b := make([]byte, 8)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return binary.NativeEndian.Uint64(b)
 }
